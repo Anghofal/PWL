dda724fb0a4f99acf141e0969d5b12b7ca4e5f28
Anghofal <asutedjaww@gmail.com>
2022-04-19 20:24:23 +0700

    UTS

---
+++ UTS/deleteBarang.php
@@ -0,0 +1,7 @@
+<h1>Silahkan Delete Barang</h1>
+<form action="" method="POST">
+			<p style="font-size: 2rem; font-weight: 800;">Delete</p>
+			<div >
+				<input type="text" placeholder="kode_barang" name="kode_barang">
+			</div>
+		</form>
---
+++ UTS/updateBarang.php
@@ -0,0 +1,22 @@
+<h1>Silahkan Update Barang</h1>
+<form action="" method="POST">
+			<p style="font-size: 2rem; font-weight: 800;">Update</p>
+			<div >
+				<input type="text" placeholder="kode_barang" name="kode_barang">
+			</div>
+			<div>
+				<input type="text" placeholder="nama" name="nama">
+			</div>
+            <div>
+				<input type="text" placeholder="harga" name="harga">
+			</div>
+            <div>
+				<input type="text" placeholder="gambar" name="gambar">
+			</div>
+            <div>
+				<input type="number" placeholder="jumlah Stok" name="jml_stok">
+			</div>
+			<div>
+				<button name="submit" >Masukkan</button>
+			</div>
+		</form>
